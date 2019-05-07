8ea581431dee30776a063a391f6ac1b3269e0d45
gepopp <office@sewemo.eu>
2019-05-07 23:10:15 +0200

    resp

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,10 +14,12 @@
     </head>
     <body>
         <div class="container-fluid intro" style="position:relative;background-image: url({{ asset('img/raum_manufaktur_home_eingang1920.jpg') }}); background-repeat: no-repeat; background-size: auto; background-position: top center;">
-            <div style="position: absolute; bottom: -50px" class="col-md-6 col-8 offset-2 offset-md-6">
-                <div class="shadow-lg" style="position: absolute; left: -50px; bottom: -50px; background-color: #00c2de; width: 80px; height: 90%"></div>
-                <img src="{{ asset("img/logo_banner.png") }}" class="img-fluid shadow-lg">
-            </div>
+           <div class="row">
+               <div style="position: absolute; bottom: -50px" class="col-md-6 col-8 offset-2 offset-md-6">
+                   <div class="shadow-lg" style="position: absolute; left: -50px; bottom: -50px; background-color: #00c2de; width: 80px; height: 90%"></div>
+                   <img src="{{ asset("img/logo_banner.png") }}" class="img-fluid shadow-lg">
+               </div>
+           </div>
         </div>
             <div class="container mt-5">
                 <div class="row">
