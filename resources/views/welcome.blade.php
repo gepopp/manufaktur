d3c22c1f7d0475c2451d4490180179c394a0d0d7
gepopp <office@sewemo.eu>
2019-05-07 22:36:09 +0200

    first Layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,9 +14,9 @@
     </head>
     <body>
         <div class="container-fluid" style="height: 100vh; background-image: url({{ asset('img/raum_manufaktur_home_eingang1920.jpg') }}); background-repeat: no-repeat">
-            <div style="position: absolute; left: 45%; bottom: -50px; background-color: #00c2de; width: 10%; height: 500px"></div>
-            <div style="position: absolute; left: 50%; bottom: -50px" class="shadow-lg">
-                <img src="{{ asset("img/logo_banner.png") }}" class="img-fluid w-75">
+            <div class="shadow-lg" style="position: absolute; left: 45%; bottom: -50px; background-color: #00c2de; width: 10%; height: 400px"></div>
+            <div style="position: absolute; left: 50%; bottom: -50px">
+                <img src="{{ asset("img/logo_banner.png") }}" class="img-fluid w-75 shadow-lg">
             </div>
         </div>
         <div class="container-fluid">
@@ -27,8 +27,8 @@
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-6 offset-6">
-                        <h1 style="color:#00c2de">Lösungen für Spezialreinigungen</h1>
+                    <div class="col-12 col-md-6 offset-md-6">
+                        <h1 class="h1" style="color:#00c2de">Lösungen für Spezialreinigungen</h1>
                         <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
                     </div>
                 </div>
